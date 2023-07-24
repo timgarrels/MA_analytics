9f93697a824e13a52b40a6018d6b81b131d5ffc3
Tim Garrels <tim.garrels@protonmail.com>
2023-07-24 14:10:47 +0100

    Weightbased and Given Module PMetrics

---
+++ custom_pmetrics/GivenModuleParticipation.py
@@ -0,0 +1,41 @@
+"""PMetric to calculate the number of predefined graph modules a graphlet touches.
+ONLY WORKS ON THE ORIGINAL GRAPH!"""
+from typing import List, Dict
+import networkx as nx
+from networkx.algorithms.community import greedy_modularity_communities
+
+from pmotif_lib.p_metric.p_metric import PMetric, PreComputation
+
+
+class GivenModuleParticipation(PMetric):
+    """Measures how many unique graph modules a graphlet participates in.
+    Graph modules are previously known.
+    A graphlet participates in a module, if at least one graphlet node belongs to that module.
+
+    This metrics relies on predefined modules, and therefore can not be calculated on random graphs!
+    (as the predefined modules lose their meaning during the randomization of edges)
+    """
+
+    def __init__(self, modules: List[List[str]]):
+        self.modules = modules
+        super().__init__("pGivenModuleParticipation")
+
+    def pre_computation(self, graph: nx.Graph) -> PreComputation:
+        """No precomputation needed, as modules are given."""
+        return {}
+
+    def metric_calculation(
+        self,
+        graph: nx.Graph,
+        graphlet_nodes: List[str],
+        pre_compute: PreComputation,
+    ) -> List[int]:
+        """Returns a list of indices
+        indicating which modules contain nodes of the graphlet occurrence."""
+        participations = []
+        for i, graph_module in enumerate(self.modules):
+            for node in graphlet_nodes:
+                if node in graph_module:
+                    participations.append(i)
+                    break
+        return participations
---
+++ custom_pmetrics/InternalWeight.py
@@ -0,0 +1,42 @@
+"""ONLY WORKS ON THE ORIGINAL GRAPH!"""
+from typing import List
+
+import networkx as nx
+from pmotif_lib.p_metric import p_metric
+from pmotif_lib.p_metric.p_metric import PreComputation
+
+
+class InternalWeight(p_metric.PMetric):
+    """PMetric to gather the weight of all edges within the graphlet occurrence"""
+    def __init__(self, graph_with_weights: nx.Graph, data_key="weight"):
+        """GtrieScanner does not allow weights. Therefore, the edgelists handed to the pmotif lib never contain
+        (meaningful) weights. This makes a side-channel to inject the weights necessary.
+        Providing the same edgelist with weights for this metric is said side-channel."""
+        self.graph_with_weights = graph_with_weights
+        self.data_key = data_key
+
+        super().__init__("pInternalWeight")
+
+    def metric_calculation(
+        self,
+        graph: nx.Graph,
+        graphlet_nodes: List[str],
+        pre_compute: PreComputation,
+    ) -> List[float]:
+        """Gathers the edge weight of every edge within the graphlet."""
+        nodes = set(graphlet_nodes)
+
+        internal_edges = [
+            (src, dst)
+            for src, dst in graph.edges(graphlet_nodes)
+            if src in nodes and dst in nodes
+        ]
+
+        return [
+            self.graph_with_weights.get_edge_data(u, v)[self.data_key]
+            for u, v in internal_edges
+        ]
+
+    def pre_computation(self, graph: nx.Graph) -> PreComputation:
+        """No pre-computation necessary for weight calculation."""
+        return {}
